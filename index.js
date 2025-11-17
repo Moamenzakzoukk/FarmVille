b1d06ffca6e6215cb39773c9f869650c7c4e8a9f
moamenzakzoukk <moamenzakzoukk0@gmail.com>
2025-11-18 00:14:06 +0200

    updated mongo to cloud

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 mongoose.set('strictQuery', true)
 
- mongoose.connect('mongodb://127.0.0.1:27017/smartagriDB', {
+ mongoose.connect('mongodb+srv://Mzakzouk:zakzouk0@cluster0.i22llgo.mongodb.net/smartagriDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true
  })
